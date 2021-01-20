703d1e02318e184aaf7f1217dcf83de32cbb68cd
mlevitas94 <levitasmc@gmail.com>
2021-01-19 23:53:03 -0500

    full report desktop responsive

--- src/pages/FullReport/FullReport.js
+++ src/pages/FullReport/FullReport.js
@@ -5,87 +5,91 @@ import './FullReport.scss'
 const FullReport = () => {
     return (
         <div className='fullReportCont'>
-            <div className='header'>
-                <h3>Mon, July 6</h3>
-                <div className='tempType'>C º</div>
-            </div>
-            <div className='mainInfo'>
-                <div className='infoColumnLeft'>
-                    <h4>City</h4>
-                    <span className='temp'>33º</span>
-                    <div className='highLow'>
-                        <span className='high'>44</span>
-                        <span className='low'>22</span>
+            <div className='fullReportFlex'>
+
+                <div className='header'>
+                    <h3>Mon, July 6 <span className='tempType'>C º</span></h3>
+                </div>
+                <div className='mainInfo'>
+                    <div className='infoColumnLeft'>
+                        <h4>City</h4>
+                        <span className='temp'>33º</span>
+                        <div className='highLow'>
+                            <span className='high'>44</span>
+                            <span className='low'>22</span>
+                        </div>
+                        <div className='description'>
+                            Sunny
                     </div>
-                    <div className='description'>
-                        Sunny
+                    </div>
+                    <div className='divider'></div>
+                    <div className='infoColumnRight'>
+                        <div>
+                            <span>Sunrise</span>
+                            <span>3:55 AM</span>
+                        </div>
+                        <div>
+                            <span>Sunrise</span>
+                            <span>3:55 AM</span>
+                        </div>
+                        <div>
+                            <span>Sunrise</span>
+                            <span>3:55 AM</span>
+                        </div>
+                        <div>
+                            <span>Sunrise</span>
+                            <span>3:55 AM</span>
+                        </div>
+                        <div>
+                            <span>Sunrise</span>
+                            <span>3:55 AM</span>
+                        </div>
+                        <div>
+                            <span>Sunrise</span>
+                            <span>3:55 AM</span>
+                        </div>
                     </div>
                 </div>
-                <div className='infoColumnRight'>
+                <div className='hourly'>
                     <div>
-                        <span>Sunrise</span>
-                        <span>3:55 AM</span>
+                        <span>Now</span>
+                        <span>28 º</span>
                     </div>
                     <div>
-                        <span>Sunrise</span>
-                        <span>3:55 AM</span>
+                        <span>Now</span>
+                        <span>28 º</span>
                     </div>
                     <div>
-                        <span>Sunrise</span>
-                        <span>3:55 AM</span>
+                        <span>Now</span>
+                        <span>28 º</span>
                     </div>
                     <div>
-                        <span>Sunrise</span>
-                        <span>3:55 AM</span>
+                        <span>Now</span>
+                        <span>28 º</span>
                     </div>
                     <div>
-                        <span>Sunrise</span>
-                        <span>3:55 AM</span>
-                    </div>
-                    <div>
-                        <span>Sunrise</span>
-                        <span>3:55 AM</span>
+                        <span>Now</span>
+                        <span>28 º</span>
                     </div>
                 </div>
-            </div>
-            <div className='hourly'>
-                <div>
-                    <span>Now</span>
-                    <span>28 º</span>
+                <div className='threeDay'>
+                    <div className='row'>
+                        <span>Tuesday</span>
+                        <span className='icon'>%</span>
+                        <div className='highLow'><span>44</span> <span>22</span></div>
+                    </div>
+                    <div className='row'>
+                        <span>Tuesday</span>
+                        <span>%</span>
+                        <div className='highLow'><span>44</span> <span>22</span></div>
+                    </div>
+                    <div className='row'>
+                        <span>Tuesday</span>
+                        <span>%</span>
+                        <div className='highLow'><span>44</span> <span>22</span></div>
+                    </div>
                 </div>
-                <div>
-                    <span>Now</span>
-                    <span>28 º</span>
-                </div>
-                <div>
-                    <span>Now</span>
-                    <span>28 º</span>
-                </div>
-                <div>
-                    <span>Now</span>
-                    <span>28 º</span>
-                </div>
-                <div>
-                    <span>Now</span>
-                    <span>28 º</span>
-                </div>
-            </div>
-            <div className='threeDay'>
-                <div className='row'>
-                    <span>Tuesday</span>
-                    <span>%</span>
-                    <div className='highLow'><span>44</span> <span>22</span></div>
-                </div>
-                <div className='row'>
-                    <span>Tuesday</span>
-                    <span>%</span>
-                    <div className='highLow'><span>44</span> <span>22</span></div>
-                </div>
-                <div className='row'>
-                    <span>Tuesday</span>
-                    <span>%</span>
-                    <div className='highLow'><span>44</span> <span>22</span></div>
-                </div>
+
             </div>
 
         </div>
